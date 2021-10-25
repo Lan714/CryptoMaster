dd14da18453af3c7d74a2aeff8a7db32d1fdf47c
jypkr <wodusqkr1210@gmail.com>
2021-10-24 21:54:43 -0700

    add year in home display

--- client/src/components/BalanceDisplay/BalanceDisplay.js
+++ client/src/components/BalanceDisplay/BalanceDisplay.js
@@ -17,7 +17,7 @@ const BalanceDisplay = () => {
 	return (
 		<Container className="text-center fw-bold text-white" id="balanceDisplayCont">
 			<Card className="bg-dark border">
-				<Card.Body>Week{Ingame_weekNumber().ingame_weeknumber} Cash Balance : ${balance}</Card.Body>
+				<Card.Body>{Ingame_weekNumber().currentdate.getFullYear()} Week {Ingame_weekNumber().ingame_weeknumber} Cash Balance : ${balance}</Card.Body>
 			</Card>
 		</Container>
 	)
